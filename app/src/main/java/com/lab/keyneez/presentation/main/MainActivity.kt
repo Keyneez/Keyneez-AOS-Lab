45fbd5da5454a2afe278d69cdf7c7fc7f726b4f0
b1urrrr <b1urrr@naver.com>
2022-12-29 01:35:49 +0900

    [FEAT/#6] bottom navigation bar 프래그먼트 전환 구현

--- app/src/main/java/com/lab/keyneez/presentation/main/MainActivity.kt
+++ app/src/main/java/com/lab/keyneez/presentation/main/MainActivity.kt
@@ -1,8 +1,15 @@
 package com.lab.keyneez.presentation.main
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 import com.lab.keyneez.R
 import com.lab.keyneez.databinding.ActivityMainBinding
+import com.lab.keyneez.presentation.main.card.CardFragment
+import com.lab.keyneez.presentation.main.home.HomeFragment
+import com.lab.keyneez.presentation.main.like.LikeFragment
+import com.lab.keyneez.presentation.main.setting.SettingFragment
 import com.lab.keyneez.util.binding.BindingActivity
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -10,6 +17,30 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        initBottomNavigationBar()
+    }
+
+    private fun initBottomNavigationBar() {
+        // 초기 프래그먼트 설정
+        val currentFragment = supportFragmentManager.findFragmentById(R.id.container_main)
+        if (currentFragment == null) navigateTo<HomeFragment>()
+
+        // 메뉴 클릭 시 프래그먼트 전환
+        binding.botNavMain.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.menu_home -> navigateTo<HomeFragment>()
+                R.id.menu_like -> navigateTo<LikeFragment>()
+                R.id.menu_card -> navigateTo<CardFragment>()
+                R.id.menu_setting -> navigateTo<SettingFragment>()
+            }
+            true
+        }
+    }
+
+    private inline fun <reified T : Fragment> navigateTo() {
+        supportFragmentManager.commit {
+            replace<T>(R.id.container_main, T::class.java.canonicalName)
+        }
     }
 }
