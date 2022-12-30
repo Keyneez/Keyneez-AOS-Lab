17fcabade3d9e436a2329bf9be70fca4be170d4d
0zlrlo <99941493+0zlrlo@users.noreply.github.com>
2022-12-30 14:46:01 +0900

    [UI/#4] 코드 리뷰 반영

--- app/src/main/java/com/lab/keyneez/presentation/main/like/LikeFragment.kt
+++ app/src/main/java/com/lab/keyneez/presentation/main/like/LikeFragment.kt
@@ -2,22 +2,22 @@ package com.lab.keyneez.presentation.main.like
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.viewModels
 import com.lab.keyneez.R
 import com.lab.keyneez.databinding.FragmentLikeBinding
 import com.lab.keyneez.util.binding.BindingFragment
 
 class LikeFragment : BindingFragment<FragmentLikeBinding>(R.layout.fragment_like) {
-    private var _binding: FragmentLikeBinding? = null
-        get() = requireNotNull(_binding) { "binding value is null." }
+    // newinstance() 사용하기
+    private val likeViewModel by viewModels<LikeViewModel>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initAdapter()
         // 여기서 코드 작성
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    private fun initAdapter() {
     }
 
     companion object {
