9801d9ef27937359cb3f84c8212c3af076b66bfe
b1urrrr <b1urrr@naver.com>
2023-01-01 02:42:02 +0900

    [UI/#12] verification 뷰 구현

--- app/src/main/java/com/lab/keyneez/presentation/main/card/CardFragment.kt
+++ app/src/main/java/com/lab/keyneez/presentation/main/card/CardFragment.kt
@@ -1,14 +1,26 @@
 package com.lab.keyneez.presentation.main.card
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import com.lab.keyneez.R
 import com.lab.keyneez.databinding.FragmentCardBinding
+import com.lab.keyneez.presentation.verification.VerificationActivity
 import com.lab.keyneez.util.binding.BindingFragment
+import com.lab.keyneez.util.extension.setOnSingleClickListener
 
 class CardFragment : BindingFragment<FragmentCardBinding>(R.layout.fragment_card) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        initIssueBtnClickListener()
+    }
+
+    private fun initIssueBtnClickListener() {
+        binding.btnCardIssue.setOnSingleClickListener {
+            val toVertification = Intent(activity, VerificationActivity::class.java)
+            startActivity(toVertification)
+        }
     }
 
     companion object {
