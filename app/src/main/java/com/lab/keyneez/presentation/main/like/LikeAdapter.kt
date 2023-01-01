293fbc6fccc331d6813990f3aabbb1bdada1a9f8
0zlrlo <99941493+0zlrlo@users.noreply.github.com>
2023-01-01 16:35:27 +0900

    [FEAT/#11] 저장 탭 구현

--- app/src/main/java/com/lab/keyneez/presentation/main/like/LikeAdapter.kt
+++ app/src/main/java/com/lab/keyneez/presentation/main/like/LikeAdapter.kt
@@ -1,6 +1,5 @@
 package com.lab.keyneez.presentation.main.like
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,12 +10,13 @@ import com.bumptech.glide.Glide
 import com.lab.keyneez.R
 import com.lab.keyneez.data.entity.LikeData
 
-class LikeAdapter(private val context: Context) :
+class LikeAdapter :
     RecyclerView.Adapter<LikeAdapter.ViewHolder>() {
     var data = mutableListOf<LikeData>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(context).inflate(R.layout.item_like_content, parent, false)
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_like_content, parent, false)
         return ViewHolder(view)
     }
 
@@ -26,7 +26,7 @@ class LikeAdapter(private val context: Context) :
         holder.bind(data[position])
     }
 
-    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
         private val imgBackground: ImageView = itemView.findViewById(R.id.iv_like_background)
         private val tvDate: TextView = itemView.findViewById(R.id.tv_like_date)
@@ -34,7 +34,7 @@ class LikeAdapter(private val context: Context) :
 
         fun bind(item: LikeData) {
             Glide.with(itemView).load(item.background).into(imgBackground)
-            tvDate.text = item.date.toString()
+            tvDate.text = item.date
             tvTitle.text = item.title
         }
     }
