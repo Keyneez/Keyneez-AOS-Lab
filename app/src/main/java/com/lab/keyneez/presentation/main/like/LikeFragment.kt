5535621e85ecc347a28ffa5648156356b7c5df11
0zlrlo <99941493+0zlrlo@users.noreply.github.com>
2023-01-03 13:35:56 +0900

    [ADD/#17] 뷰모델을 이용하여 구현

--- app/src/main/java/com/lab/keyneez/presentation/main/like/LikeFragment.kt
+++ app/src/main/java/com/lab/keyneez/presentation/main/like/LikeFragment.kt
@@ -2,6 +2,7 @@ package com.lab.keyneez.presentation.main.like
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
 import com.lab.keyneez.R
 import com.lab.keyneez.data.entity.LikeData
@@ -13,19 +14,39 @@ class LikeFragment : BindingFragment<FragmentLikeBinding>(R.layout.fragment_like
     // private val likeViewModel by viewModels<LikeViewModel>()
     lateinit var likeAdapter: LikeAdapter
     val data = mutableListOf<LikeData>()
+    val viewModel: LikeViewModel by viewModels()
+    // liveData는 observer 패턴을 기반으로 하며 뷰모델과 뷰 간의 통신을 쉽게 한다.
+    // 또한 데이터 변경을 관찰하고 데이터를 자동으로 업데이트한다.
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initLikeAdapter()
-        initLikeClickListener()
+        initLikeSaveClickListener()
+        initLikeSearchClickListener()
+        setupLikeData()
+    }
+
+    private fun initLikeSearchClickListener() {
         binding.btnLikeSearch.setOnClickListener {
             // 검색화면으로 이동
         }
     }
 
-    private fun initLikeClickListener() {
+    private fun initLikeSaveClickListener() {
         binding.btnLikeSave.setOnClickListener {
             binding.rvLikeContent.smoothScrollToPosition(0)
+        }
+    }
+
+    private fun setupLikeData() {
+        // ViewModel을 observe하는 부분을 사용하면 된다.
+        // 뷰모델에서 likelist가 바뀔 때마다 자동적으로 호출되어 바꿔줌
+        // viewLifecycleOwner 대신에 액티비티에서는 this를 사용해 준다.
+        // 상태 변경을 자동으로 알린다.
+        viewModel.likeList.observe(viewLifecycleOwner) { it ->
+            // 어댑터에서 mutablelist썼으면 뷰홀더에서도 mutablelist를 사용하고 어댑터에서 listOf를 썼으면 뷰홀더에서도 통일하기
+            likeAdapter.data = it
+            likeAdapter.notifyDataSetChanged()
         }
     }
 
@@ -33,53 +54,6 @@ class LikeFragment : BindingFragment<FragmentLikeBinding>(R.layout.fragment_like
         likeAdapter = LikeAdapter()
         binding.rvLikeContent.adapter = likeAdapter
         binding.rvLikeContent.layoutManager = GridLayoutManager(activity, 2)
-        data.apply {
-            add(
-                LikeData(
-                    background = R.drawable.img_like_background,
-                    date = "12.4.12-10",
-                    title = "청소년 영화관 할인"
-                )
-            )
-            add(
-                LikeData(
-                    background = R.drawable.img_like_background,
-                    date = "12.11-12.17",
-                    title = "청소년 미술관 할인"
-                )
-            )
-            add(
-                LikeData(
-                    background = R.drawable.img_like_background,
-                    date = "12.18-12.24",
-                    title = "청소년 공연장 할인"
-                )
-            )
-            add(
-                LikeData(
-                    background = R.drawable.img_like_background,
-                    date = "12.25-12.31",
-                    title = "청소년 박물관 할인"
-                )
-            )
-            add(
-                LikeData(
-                    background = R.drawable.img_like_background,
-                    date = "1.1-1.7",
-                    title = "청소년 서점 할인"
-                )
-            )
-            add(
-                LikeData(
-                    background = R.drawable.img_like_background,
-                    date = "1.8-1.14",
-                    title = "청소년 강의 할인"
-                )
-            )
-
-            likeAdapter.data = data
-            likeAdapter.notifyDataSetChanged()
-        }
     }
 
     companion object {
